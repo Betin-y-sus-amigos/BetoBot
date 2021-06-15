65a4eded2dc1c0d0af8dcd8b0fef0a530b2a3f6c
HakkinDavid <desr11@hotmail.com>
2021-06-15 02:28:47 -0700

    Update saludar.js

--- cuak/saludar.js
+++ cuak/saludar.js
@@ -4,7 +4,7 @@ function (msg, sazonador) {
   if (settingsOBJ[msg.guild.id] == undefined) {
     settingsOBJ[msg.guild.id] = {};
   }
-  let canal = sazonador.match(/<#[0-9]+>/);
+  let canal = sazonador.match(/<#[0-9]+>/).slice(2,-1);
   if (canal == null) msg.channel.send("menciona un canal.") return;
   settingsOBJ[msg.guild.id].saludo = canal;
   beto.backup('ajustes.txt', ajustes);
