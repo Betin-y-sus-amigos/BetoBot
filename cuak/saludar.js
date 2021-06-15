25b8a96f2c643b483b4291de1a2190485fc6dc64
HakkinDavid <desr11@hotmail.com>
2021-06-15 02:29:47 -0700

    Update saludar.js

--- cuak/saludar.js
+++ cuak/saludar.js
@@ -7,5 +7,6 @@ function (msg, sazonador) {
   let canal = sazonador.match(/<#[0-9]+>/).slice(2,-1);
   if (canal == null) msg.channel.send("menciona un canal.") return;
   settingsOBJ[msg.guild.id].saludo = canal;
+  msg.channel.send("saludaré a los nuevos miembros en <#" + canal + ">")
   beto.backup('ajustes.txt', ajustes);
 }
