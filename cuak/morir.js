85ef4847763236d7895d43c70906428a77c1c608
HakkinDavid <desr11@hotmail.com>
2021-06-27 22:38:16 -0700

    Update morir.js

--- cuak/morir.js
+++ cuak/morir.js
@@ -3,6 +3,7 @@ module.exports =
 function (msg) {
   if (msg.author.id != "265257341967007758" && msg.author.id != "466753794244345866") { msg.channel.send("eso es un comando de admin"); return};
   msg.channel.send('me mori');
-  client.destroy();
-  setTimeout(()=>{process.exit()},3600000)
+
+  setTimeout(()=>{client.destroy()},1000);
+  setTimeout(()=>{process.exit()},3600000);
 }
